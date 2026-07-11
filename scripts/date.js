e4b9f438097c7bedf715faf70e6bec8416d6bc7d
Burhan Hassan <afrikada.com@gmail.com>
2026-07-11 21:37:55 +0300

    W01 Course Home Page IMPRPVED

--- scripts/date.js
+++
@@ -1,9 +0,0 @@
-const currentYear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified");
-
-// Display current year
-currentYear.textContent = new Date().getFullYear();
-
-// Display last modified date
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
-
